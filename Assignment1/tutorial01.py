edfe8f5abb70c96a8f70c548095c930509ea998a
Aman Kumar <43760029+Aman1124@users.noreply.github.com>
2020-09-16 07:11:50 -0700

    Update tutorial01.py

--- Assignment1/tutorial01.py
+++ Assignment1/tutorial01.py
@@ -15,9 +15,9 @@ def multiply(num1, num2):
 
 # Function to divide two numbers 
 def divide(num1, num2): 
-	if(num2 is not 0)
+	if num2 is not 0:
 		division = num1/num2
-	else 
+	else:
 		division = "Cannot divide by 0"
 	return division
 	
