79c6602fdbc72a6ef2153efa76947fa080e23f22
Aman Kumar <amankumarthakur666@gmail.com>
2020-09-16 23:45:53 -0700

    Added 4 new functions with logic of power function defined

--- Assignment1/tutorial01.py
+++ Assignment1/tutorial01.py
@@ -20,4 +20,35 @@ def divide(num1, num2):
 	else:
 		division = "Cannot divide by 0"
 	return division
+
+# Function to add power function
+#You cant use the inbuilt python function x ** y . Write your own function
+def power(num1, num2): #num1 ^ num2
+	if isinstance(num2,int):
+		if num2 >= 1:
+			num2 = num2 - 1
+			return num1*power(num1,num2)
+		elif num2 == 0:
+			return 1
+		elif num2 < 0:
+			return 1/power(num1,num2*(-1))
+	else:
+		return "power shoud be an integer"
 	
+# Python 3 program to print GP.  geometric Progression
+#You cant use the inbuilt python function. Write your own function
+def printGP(a, r, n): 
+	gp=[]
+	return gp 
+
+# Python 3 program to print AP.  arithmetic Progression
+#You cant use the inbuilt python function. Write your own function
+def printAP(a, d, n): 
+	ap=[]
+	return ap
+
+# Python 3 program to print HP.   Harmonic Progression
+#You cant use the inbuilt python function. Write your own function
+def printHP(a, d, n): 
+	hp=[]
+	return hp
