030dcd6b473fb21b3f6d87ab855c08efaa51ac50
Aman Kumar <amankumarthakur666@gmail.com>
2020-09-23 21:19:36 +0530

    Added logic of summation

--- Assignment2/tutorial02.py
+++ Assignment2/tutorial02.py
@@ -136,4 +136,7 @@ def kurtosis(first_list):
 # Function to compute sum. You cant use Python functions
 def summation(first_list):
     # sum Logic
+    summation_value = 0
+    for x in first_list:
+        summation_value =  summation_value + x
     return summation_value
