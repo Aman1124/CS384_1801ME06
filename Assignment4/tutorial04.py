0724dd1747aba6dcd0c370b372786721aa9eba20
Aman1124 <amankumarthakur666@gmail.com>
2020-11-08 18:14:27 +0530

    Assignment 4: Added function to write data to individual CSVs

--- Assignment4/tutorial04.py
+++ Assignment4/tutorial04.py
@@ -49,8 +49,44 @@ def create_all_csv():
 
     return roll_no
 
+def write_data_individual(roll_no_data):
+	with open("acad_res_stud_grades.csv", "r") as file:
+		reader = csv.reader(file)
+		p = 0
+		c = 1
+		data = []
+		for row in reader:
+			if c == 1:
+				c = 2
+				continue
+			data.append(row)
+
+		i = 0
+		while i < len(data):	
+			if data[i][6] in validGrades:
+				if roll_no_data[p] == data[i][1]:
+					file1 = open(currPath + "/" + roll_no_data[p] + "_individual.csv", "a")
+					writer = csv.writer(file1)
+					rowData = []
+					rowData = data[i][4:6]
+					rowData.append(data[i][8])
+					rowData.append(data[i][6])
+					rowData.append(data[i][2])
+					writer.writerow(rowData)
+				else:
+					i -= 1
+					p += 1
+			else:
+				file1 = open(currPath + "/" + "misc.csv", "a")
+				writer = csv.writer(file1)
+				writer.writerow(data[i])
+			i += 1
+
+
 
 del_create_grades_folder()
 print("del_create_grades_folder() completed")
 roll_no = create_all_csv()
 print("create_all_csv() completed")
+write_data_individual(roll_no)
+print("write_data_individual() completed")
